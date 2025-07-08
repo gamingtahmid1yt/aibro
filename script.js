f1036bd0d0922bd099ee93163862d211dc34c66e
gamingtahmid1yt <gamingtahmid08@gmail.com>
2025-07-08 16:58:16 +0600

    script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ function showAccountPanel() {
   accountUsername.textContent = currentUser.username;
   accountStatus.textContent = currentUser.isPremium ? 'Premium User' : 'Free User';
   accountStatus.className = currentUser.isPremium ? 'tag premium' : 'tag free';
-  adminBtn.style.display = currentUser.username.toLowerCase() === 'tahmid' ? 'block' : 'none';
+  adminBtn.style.display = currentUser.username.toLowerCase() === 'tahmid', 'developer' ? 'block' : 'none';
   accountPanel.classList.add('show');
 }
 
